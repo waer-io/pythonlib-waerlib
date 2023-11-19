da9f341a69489b3b588bc1e984bdcb464ef51fad
tanelaamer <tanel.aamer@gmail.com>
2023-11-19 19:25:18 +0100

    Add user_id log field

--- waerlib/logging_config.py
+++ waerlib/logging_config.py
@@ -8,9 +8,8 @@ import threading
     See example/logging_config.py for example usage.
 """
 
-
 local_storage = threading.local()
-undefined_request_id = "N/A"
+undefined_id = "N/A"
 
 
 class StructuredFormatter(logging.Formatter):
@@ -29,7 +28,8 @@ class StructuredFormatter(logging.Formatter):
             'message': record.getMessage(),
             'pathname': record.pathname,
             'lineno': record.lineno,
-            'request_id': get_request_id()
+            'request_id': get_request_id(),
+            'user_id': get_user_id()
         }
 
         # Exception info, if there is any
@@ -49,17 +49,34 @@ def setup_logging():
 
 
 def get_request_id():
-    return getattr(local_storage, 'request_id', undefined_request_id)
+    return getattr(local_storage, 'request_id', undefined_id)
+
+
+def get_user_id():
+    return getattr(local_storage, 'user_id', undefined_id)
 
 
 def get_request_id_header_key():
     return "X-Request-ID"
 
 
+def get_user_id_key():
+    return "user_id"
+
+
 def store_request_id(request_id):
     local_storage.request_id = request_id
+
+
+def store_user_id(user_id):
+    local_storage.user_id = user_id
 
 
 def del_current_request_id():
     if hasattr(local_storage, 'request_id'):
         del local_storage.request_id
+
+
+def del_current_user_id():
+    if hasattr(local_storage, 'user_id'):
+        del local_storage.user_id

ae9bdd419a959815dcbaf7fab337e69b1850707b
tanelaamer <tanel.aamer@gmail.com>
2024-02-03 20:00:24 +0100

    Move refresh collections

---
+++ waerlib/refresh.py
@@ -0,0 +1,43 @@
+
+import os
+from datetime import datetime
+import pyarrow.flight as flight
+
+tag_refresh = "[WAERLIB: refresh]"
+
+
+def refresh_collections(collections = ['profiles', 'outputs', 'samples', 'parsed']):
+    """
+    Refreshes metadata for specified collections in a Dremio datalake using Apache Arrow Flight.
+
+    Ensures we get latest data.
+    """
+
+    print(f"[{datetime.now()}] {tag_refresh} Starting refresh collections...")
+
+    host = os.environ['DREMIO_HOST']
+    username = os.environ['DREMIO_USERNAME']
+    password = os.environ['DREMIO_PASSWORD']
+
+    try:
+        flight_client = flight.FlightClient(f'grpc+tcp://{host}:32010/grpc')
+
+        print(f"[{datetime.now()}] {tag_refresh} Authenticating token..")
+        token = flight_client.authenticate_basic_token(username, password)
+        print(f"[{datetime.now()}] {tag_refresh} Authenticated token")
+        options = flight.FlightCallOptions(headers=[token])
+
+        for collection in collections:
+            query = f'''ALTER TABLE datalake.{collection} REFRESH METADATA;'''
+            print(f"[{datetime.now()}] {tag_refresh} Refreshing metadata for {collection}")
+            flight_info = flight_client.get_flight_info(flight.FlightDescriptor.for_command(query), options)
+            print(f"[{datetime.now()}] {tag_refresh} Performing get for {collection}")
+            reader = flight_client.do_get(flight_info.endpoints[0].ticket, options)
+            # Process reader if needed here
+
+        print(f"[{datetime.now()}] {tag_refresh} Metadata refresh completed.")
+        return True
+
+    except Exception as e:
+        print(f"[{datetime.now()}] {tag_refresh} An error occurred refreshing metadata: {e}")
+        return False

76c3d856e6ac0aaa0c61120cefd4177463a9aacb
tanelaamer <tanel.aamer@gmail.com>
2024-03-14 22:48:44 +0100

    Update refresh_collections, print flush

--- waerlib/refresh.py
+++ waerlib/refresh.py
@@ -13,7 +13,7 @@ def refresh_collections(collections = ['profiles', 'outputs', 'samples', 'parsed
     Ensures we get latest data.
     """
 
-    print(f"[{datetime.now()}] {tag_refresh} Starting refresh collections...")
+    print(f"[{datetime.now()}] {tag_refresh} Starting refresh collections...", flush=True)
 
     host = os.environ['DREMIO_HOST']
     username = os.environ['DREMIO_USERNAME']
@@ -25,22 +25,22 @@ def refresh_collections(collections = ['profiles', 'outputs', 'samples', 'parsed
         for collection in collections:
 
             # try auth each time, so as to not expire token
-            print(f"[{datetime.now()}] {tag_refresh} Authenticating token..")
+            print(f"[{datetime.now()}] {tag_refresh} Authenticating token..", flush=True)
             token = flight_client.authenticate_basic_token(username, password)
-            print(f"[{datetime.now()}] {tag_refresh} Authenticated token")
+            print(f"[{datetime.now()}] {tag_refresh} Authenticated token", flush=True)
             options = flight.FlightCallOptions(headers=[token])
             # /
 
             query = f'''ALTER TABLE datalake.{collection} REFRESH METADATA;'''
-            print(f"[{datetime.now()}] {tag_refresh} Refreshing metadata for {collection}")
+            print(f"[{datetime.now()}] {tag_refresh} Refreshing metadata for {collection}", flush=True)
             flight_info = flight_client.get_flight_info(flight.FlightDescriptor.for_command(query), options)
-            print(f"[{datetime.now()}] {tag_refresh} Performing get for {collection}")
+            print(f"[{datetime.now()}] {tag_refresh} Performing get for {collection}", flush=True)
             reader = flight_client.do_get(flight_info.endpoints[0].ticket, options)
             # Process reader if needed here
 
-        print(f"[{datetime.now()}] {tag_refresh} Metadata refresh completed.")
+        print(f"[{datetime.now()}] {tag_refresh} Metadata refresh completed.", flush=True)
         return True
 
     except Exception as e:
-        print(f"[{datetime.now()}] {tag_refresh} An error occurred refreshing metadata: {e}")
+        print(f"[{datetime.now()}] {tag_refresh} An error occurred refreshing metadata: {e}", flush=True)
         return False

9f0b84a952cea59c055c81d2dff62446624cd8dc
tanelaamer <tanel.aamer@gmail.com>
2024-03-16 18:51:22 +0100

    Update refresh_collections, new way to refresh

--- waerlib/refresh.py
+++ waerlib/refresh.py
@@ -6,13 +6,52 @@ import pyarrow.flight as flight
 tag_refresh = "[WAERLIB: refresh]"
 
 
+
 def refresh_collections(collections = ['profiles', 'outputs', 'samples', 'parsed']):
+    """
+    Refreshes metadata for specified collections in a Dremio datalake using Apache Arrow Flight.
+    """
+    print(f"[{datetime.now()}] {tag_refresh} Starting refresh collections (new)...", flush=True)
+
+    host = os.environ['DREMIO_HOST']
+    username = os.environ['DREMIO_USERNAME']
+    password = os.environ['DREMIO_PASSWORD']
+
+    try:
+        # Establish the FlightClient outside the loop for reusing connections
+        flight_client = flight.FlightClient(f'grpc+tcp://{host}:32010/grpc')
+
+        # Authenticate token once outside the loop
+        print(f"[{datetime.now()}] {tag_refresh} Authenticating token..", flush=True)
+        token = flight_client.authenticate_basic_token(username, password)
+        print(f"[{datetime.now()}] {tag_refresh} Authenticated token", flush=True)
+        options = flight.FlightCallOptions(headers=[token], timeout=60 * 15)  # 15min
+
+        for collection in collections:
+            query = f'ALTER TABLE datalake.{collection} REFRESH METADATA;'
+            print(f"[{datetime.now()}] {tag_refresh} Refreshing metadata for {collection}", flush=True)
+
+            try:
+                result = flight_client.do_action(query, options=options)
+                print(f"[{datetime.now()}] {tag_refresh} Metadata refreshed for {collection}.", flush=True)
+            except Exception as e:
+                print(f"[{datetime.now()}] {tag_refresh} Error refreshing metadata for {collection}: {e}", flush=True)
+
+        print(f"[{datetime.now()}] {tag_refresh} Metadata refresh completed.", flush=True)
+        return True
+
+    except Exception as e:
+        print(f"[{datetime.now()}] {tag_refresh} An error occurred refreshing metadata: {e}", flush=True)
+        return False
+
+
+
+def refresh_collections_old(collections = ['profiles', 'outputs', 'samples', 'parsed']):
     """
     Refreshes metadata for specified collections in a Dremio datalake using Apache Arrow Flight.
 
     Ensures we get latest data.
     """
-
     print(f"[{datetime.now()}] {tag_refresh} Starting refresh collections...", flush=True)
 
     host = os.environ['DREMIO_HOST']
@@ -44,3 +83,5 @@ def refresh_collections(collections = ['profiles', 'outputs', 'samples', 'parsed
     except Exception as e:
         print(f"[{datetime.now()}] {tag_refresh} An error occurred refreshing metadata: {e}", flush=True)
         return False
+
+

c5d48be01906cbdbbecc2f84bf41c0810ba97cdb
tanelaamer <tanel.aamer@gmail.com>
2024-03-30 21:03:51 +0100

    Create read_new for reading data

--- waerlib/read.py
+++ waerlib/read.py
@@ -69,18 +69,23 @@ def read_nanos(user_id, start_time_nanos, end_time_nanos, start_year_month, end_
     token = client.authenticate_basic_token(username, password)
     options = flight.FlightCallOptions(headers=[token])
 
+
+    # the old records seem to be microseconds, so /1000 as we use nanos everywhere for post 2024MAR29 logic.
     query = f'''
     SELECT * FROM datalake.{collection}
     WHERE "dir0"='user_id={user_id}'
     AND RIGHT("dir1",7)>='{start_year_month}'
     AND RIGHT("dir1",7)<='{end_year_month}'
     AND "key" IN ({','.join(["'" + i + "'" for i in tags])})
-    AND "timestamp">='{start_time_nanos}'
-    AND "timestamp"<='{end_time_nanos}'
+    AND "timestamp">={start_time_nanos/1000}
+    AND "timestamp"<={end_time_nanos/1000}
     '''
 
     flight_info = client.get_flight_info(flight.FlightDescriptor.for_command(query), options)
     reader = client.do_get(flight_info.endpoints[0].ticket, options)
+
+    print(f"WAERLIB OUTGOING QUERY", flush=True)
+    print(f"{query}", flush=True)
 
     df = reader.read_pandas()
     df = df.rename(columns={'dir0':'user_id'})
@@ -91,5 +96,5 @@ def read_nanos(user_id, start_time_nanos, end_time_nanos, start_year_month, end_
 
     # we'll assume we always just store nanos. (will remove this line and comment later, here for documentation purposes)
     # df.timestamp = pd.to_datetime(df.timestamp*1000)
-    return df
 
+    return df
